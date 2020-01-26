f03339638764f4cc503153dfb274726470c076af
Jerwin <jerwinfernandez04@gmail.com>
2020-01-26 20:43:42 +0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+// import gulp module
+const gulp = require('gulp');
+
+/*
+  -- TOP LEVEL FUNCTION --
+  gulp.task - define tasks
+  gulp.src - point to files to use
+  gulp.dest - point to the folder to output
+  gulp.watch - watch files and folders for changes
+*/
